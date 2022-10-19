82078b4ecccca6c2a71864b8c9331bf11739be3d
ivan <ivanmassenzio@gmail.com>
2022-10-19 19:34:48 -0300

    desafio2

--- desafio2/contenedor.js
+++ desafio2/contenedor.js
@@ -13,7 +13,7 @@ class Contenedor {
       error.code === "ENOENT"
         ? this._createEmptyFile()
         : console.log(
-            `Error Code: ${error.code} | There was an unexpected error when trying to read ${this._filename}`
+            `Codigo de error: ${error.code} | Hubo un error inesperado al intentar leer ${this._filename}`
           );
     }
   }
@@ -22,7 +22,7 @@ class Contenedor {
     fs.writeFile(this._filename, "[]", (error) => {
       error
         ? console.log(error)
-        : console.log(`File ${this._filename} was created since it didn't exist in the system`);
+        : console.log(`Archivo ${this._filename} Fue creado ya que no existia en el sistema`);
     });
   }
 
@@ -34,7 +34,7 @@ class Contenedor {
       return parsedData.find((producto) => producto.id === id);
     } catch (error) {
       console.log(
-        `Error Code: ${error.code} | There was an error when trying to get an element by its ID (${id})`
+        `Codigo de error: ${error.code} | Hubo un error al intentar obtener un elemento por su ID (${id})`
       );
     }
   }
@@ -52,12 +52,12 @@ class Contenedor {
         parsedData.splice(index, 1);
         await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
       } else {
-        console.log(`ID ${id} does not exist in the file`);
+        console.log(`ID ${id} no existe en el archivo`);
         return null;
       }
     } catch (error) {
       console.log(
-        `Error Code: ${error.code} | There was an error when trying to delete an element by its ID (${id})`
+        `Codigo de error: ${error.code} | Hubo un error al intentar eliminar un elemento por su ID (${id})`
       );
     }
   }
@@ -74,7 +74,7 @@ class Contenedor {
       return object.id;
     } catch (error) {
       console.log(
-        `Error Code: ${error.code} | There was an error when trying to save an element`
+        `Codigo de error: ${error.code} | Hubo un error al intentar guardar un elemento`
       );
     }
   }
@@ -84,7 +84,7 @@ class Contenedor {
       await this._createEmptyFile();
     } catch (error) {
       console.log(
-        `There was an error (${error.code}) when trying to delete all the objects`
+        `Ha ocurrido un error (${error.code}) cuando se intento eliminar todos los objetos`
       );
     }
   }
