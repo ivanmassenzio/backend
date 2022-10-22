f851d3a1a13c04d0338bea7e13cecb84fc203de2
ivan <ivanmassenzio@gmail.com>
2022-10-22 12:52:15 -0300

    push desafio3

---
+++ desafios/contenedor.js
@@ -0,0 +1,103 @@
+const fs = require("fs");
+
+class Contenedor {
+  constructor(fileName) {
+    this._filename = fileName;
+    this._readFileOrCreateNewOne();
+  }
+
+  async _readFileOrCreateNewOne() {
+    try {
+      await fs.promises.readFile(this._filename, "utf-8");
+    } catch (error) {
+      error.code === "ENOENT"
+        ? this._createEmptyFile()
+        : console.log(
+            `Codigo de error: ${error.code} | Hubo un error inesperado al intentar leer ${this._filename}`
+          );
+    }
+  }
+
+  async _createEmptyFile() {
+    fs.writeFile(this._filename, "[]", (error) => {
+      error
+        ? console.log(error)
+        : console.log(`Archivo ${this._filename} Fue creado ya que no existia en el sistema`);
+    });
+  }
+
+  async getById(id) {
+    try {
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+
+      return parsedData.find((producto) => producto.id === id);
+    } catch (error) {
+      console.log(
+        `Codigo de error: ${error.code} | Hubo un error al intentar obtener un elemento por su ID (${id})`
+      );
+    }
+  }
+
+  async deleteById(id) {
+    try {
+      const data = await this.getData();
+      const parsedData = JSON.parse(data);
+      const objectIdToBeRemoved = parsedData.find(
+        (producto) => producto.id === id
+      );
+
+      if (objectIdToBeRemoved) {
+        const index = parsedData.indexOf(objectIdToBeRemoved);
+        parsedData.splice(index, 1);
+        await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+      } else {
+        console.log(`ID ${id} no existe en el archivo`);
+        return null;
+      }
+    } catch (error) {
+      console.log(
+        `Codigo de error: ${error.code} | Hubo un error al intentar eliminar un elemento por su ID (${id})`
+      );
+    }
+  }
+
+  async save(object) {
+    try {
+      const allData = await this.getData();
+      const parsedData = JSON.parse(allData);
+
+      object.id = parsedData.length + 1;
+      parsedData.push(object);
+
+      await fs.promises.writeFile(this._filename, JSON.stringify(parsedData));
+      return object.id;
+    } catch (error) {
+      console.log(
+        `Codigo de error: ${error.code} | Hubo un error al intentar guardar un elemento`
+      );
+    }
+  }
+
+  async deleteAll() {
+    try {
+      await this._createEmptyFile();
+    } catch (error) {
+      console.log(
+        `Ha ocurrido un error (${error.code}) cuando se intento eliminar todos los objetos`
+      );
+    }
+  }
+
+  async getData() {
+    const data = await fs.promises.readFile(this._filename, "utf-8");
+    return data;
+  }
+
+  async getAll() {
+    const data = await this.getData();
+    return JSON.parse(data);
+  }
+}
+
+module.exports = Contenedor;
